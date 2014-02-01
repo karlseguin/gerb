057935497e740e8b136b2e41b7d5fce6c0c4156a
Karl Seguin <karl@openmymind.io>
2014-02-01 13:49:41 +0800

    range support to for loop

--- for.go
+++ for.go
@@ -3,6 +3,7 @@ package gerb
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"github.com/karlseguin/gerb/core"
 )
 
@@ -59,7 +60,40 @@ func ExplicitForFactory(p *core.Parser) (core.Code, error) {
 }
 
 func RangedForFactory(p *core.Parser) (core.Code, error) {
-	return nil, nil
+	code := &RangedForCode{NormalContainer: new(core.NormalContainer)}
+	tokens, err := p.ReadTokenList()
+	if err != nil {
+		return nil, err
+	}
+	if len(tokens) != 2 {
+		return nil, p.Error("Invalid for loop, ranged loop should have two variables")
+	}
+	code.tokens = tokens
+	c := p.SkipSpaces()
+	if c == ':' {
+		c = p.Next()
+	}
+	if c != '=' {
+		return nil, p.Error("Invalid for loop, ranged loop expecting assignment operator")
+	}
+	p.Next()
+
+	if p.SkipSpaces() != 'r' || p.ConsumeIf([]byte("range")) == false {
+		return nil, p.Error("invalid for loop, expected 'range' keyword")
+	}
+
+	value, err := p.ReadValue()
+	if err != nil {
+		return nil, err
+	}
+	code.value = value
+	if p.SkipSpaces() != '{' {
+		return nil, p.Error("Missing openening brace for if statement")
+	}
+	p.Next()
+
+	return code, nil
+
 }
 
 type ForCode struct {
@@ -102,4 +136,65 @@ func (c *ForCode) IsSibling() bool {
 
 func (c *ForCode) AddCode(code core.Code) error {
 	return errors.New(fmt.Sprintf("%v is not a valid tag as a descendant of a for loop", code))
+}
+
+
+type RangedForCode struct {
+	*core.NormalContainer
+	tokens []string
+	value core.Value
+}
+
+func (c *RangedForCode) Execute(context *core.Context) core.ExecutionState {
+	value := reflect.ValueOf(c.value.Resolve(context))
+	kind := value.Kind()
+
+	if kind == reflect.Array || kind == reflect.Slice || kind == reflect.String || kind == reflect.Map {
+		length := value.Len()
+		if length == 0 {
+			return core.NormalState
+		}
+		if kind == reflect.Map {
+			c.executeMap(context, value, length)
+		} else {
+			c.executeArray(context, value, length)
+		}
+	}
+	delete(context.Data, c.tokens[0])
+	delete(context.Data, c.tokens[1])
+	return core.NormalState
+}
+
+func (c *RangedForCode) IsCodeContainer() bool {
+	return true
+}
+
+func (c *RangedForCode) IsContentContainer() bool {
+	return true
+}
+
+func (c *RangedForCode) IsSibling() bool {
+	return false
+}
+
+func (c *RangedForCode) AddCode(code core.Code) error {
+	return errors.New(fmt.Sprintf("%v is not a valid tag as a descendant of a for loop", code))
+}
+
+func (c *RangedForCode) executeArray(context *core.Context, value reflect.Value, length int) {
+	for i := 0; i < length; i++ {
+		context.Data[c.tokens[0]] = i
+		context.Data[c.tokens[1]] = value.Index(i).Interface()
+		c.NormalContainer.Execute(context)
+	}
+}
+
+func (c *RangedForCode) executeMap(context *core.Context, value reflect.Value, length int) {
+	keys := value.MapKeys()
+	for i := 0; i < length; i++ {
+		key := keys[i]
+		context.Data[c.tokens[0]] = key
+		context.Data[c.tokens[1]] = value.MapIndex(key).Interface()
+		c.NormalContainer.Execute(context)
+	}
 }
